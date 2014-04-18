f2b15f004e87797665cf3fc491e596a64e690d0e
Nicholas Audo <naudo@naudo.de>
2014-04-17 18:01:28 -0700

    Get the avg green val for a frame

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-# from moviepy.editor import VideoFileClip # for video processing
-# from pylab import * # for mathematics/plotting
-
-# # load the video, keep the clip between t=2s and t= 30s
-# video = VideoFileClip('./mp4/limehouse_nights.mp4').subclip(2,30)
-
-
-# # extract the focus lines in the different frames, stack them.
-# roll_picture = vstack([frame[[156],58:478]
-#                        for frame in video.iter_frames()])
-
-# imshow( roll_picture ) # display the obtained picture
-
 from SimpleCV import Camera
 import time
+# RGB
 cam = Camera()
+SAMPLE_WIDTH = 2
+SAMPLE_HEIGHT = SAMPLE_WIDTH
+SAMPLE_OFFSET = 0
+PIXEL_COUNT = SAMPLE_HEIGHT * SAMPLE_WIDTH
+GREEN=1
+
 while True:
     img = cam.getImage()
-    img.show()
+    img2 = img[SAMPLE_OFFSET:SAMPLE_WIDTH, SAMPLE_OFFSET:SAMPLE_HEIGHT]
+    matrix = img2.getNumpy()
+    total = 0
+    for row in range(0, SAMPLE_WIDTH):
+        for col in matrix[row]:
+            total += col[GREEN
+
+    print total / PIXEL_COUNT
+
+    img2.show()
     time.sleep(0.01)
