02c4a14f9fba75233a6cc17ea5c0080ef6b37bbb
Nicholas Audo <naudo@naudo.de>
2014-04-21 12:01:43 -0700

    Draw a capture box in the image

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from SimpleCV import Camera
+from SimpleCV import Camera, DrawingLayer
 # RGB
 cam = Camera()
 SAMPLE_WIDTH = 100
 SAMPLE_HEIGHT = SAMPLE_WIDTH
-SAMPLE_OFFSET = 0
 PIXEL_COUNT = SAMPLE_HEIGHT * SAMPLE_WIDTH
 GREEN=1
 
+
 while True:
     img = cam.getImage()
-    img2 = img[SAMPLE_OFFSET:SAMPLE_WIDTH, SAMPLE_OFFSET:SAMPLE_HEIGHT]
-    matrix = img2.getNumpy()
+    center_point = (img.width / 2, img.height / 2)
+    boxLayer = DrawingLayer((img.width, img.height))
+
+    sample_x0 = center_point[0] - (SAMPLE_WIDTH / 2)
+    sample_y0 =  center_point[1] - (SAMPLE_HEIGHT / 2)
+    
+    boxLayer.centeredRectangle(center_point, (SAMPLE_WIDTH, SAMPLE_HEIGHT))
+    sample = img[sample_x0:sample_x0 + SAMPLE_WIDTH, sample_y0: sample_y0 +SAMPLE_HEIGHT]
+
+    matrix = sample.getNumpy()
     total = 0
+
+
     for row in range(0, SAMPLE_WIDTH):
         for col in matrix[row]:
             total += col[GREEN]
 
     print total / PIXEL_COUNT
 
-    img2.show()
+    img.addDrawingLayer(boxLayer)
+    img.applyLayers()
+    img.show()
