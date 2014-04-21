4d5c4ad7cb44ac8b22fccd5004de65bbcb8d5f05
Nicholas Audo <naudo@naudo.de>
2014-04-21 12:14:07 -0700

    Some minor cleanup

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ SAMPLE_WIDTH = 100
 SAMPLE_HEIGHT = SAMPLE_WIDTH
 PIXEL_COUNT = SAMPLE_HEIGHT * SAMPLE_WIDTH
 GREEN=1
+CAM_WIDTH = int(cam.getAllProperties().get('width'))
+CAM_HEIGHT= int(cam.getAllProperties().get('height'))
 
+cam_center_point = ( CAM_HEIGHT / 2, CAM_HEIGHT / 2)
+sample_x0 = cam_center_point[0] - (SAMPLE_WIDTH / 2)
+sample_y0 =  cam_center_point[1] - (SAMPLE_HEIGHT / 2)
 
 while True:
     img = cam.getImage()
-    center_point = (img.width / 2, img.height / 2)
     boxLayer = DrawingLayer((img.width, img.height))
-
-    sample_x0 = center_point[0] - (SAMPLE_WIDTH / 2)
-    sample_y0 =  center_point[1] - (SAMPLE_HEIGHT / 2)
     
-    boxLayer.centeredRectangle(center_point, (SAMPLE_WIDTH, SAMPLE_HEIGHT))
+    boxLayer.centeredRectangle(cam_center_point, (SAMPLE_WIDTH, SAMPLE_HEIGHT))
     sample = img[sample_x0:sample_x0 + SAMPLE_WIDTH, sample_y0: sample_y0 +SAMPLE_HEIGHT]
 
     matrix = sample.getNumpy()
     total = 0
-
 
     for row in range(0, SAMPLE_WIDTH):
         for col in matrix[row]:
