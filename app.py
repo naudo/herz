5e42beb0e03297d1071149372ba66d6bfc943799
Nicholas Audo <naudo@naudo.de>
2014-04-22 14:47:55 -0700

    First stab at calculating the HB

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from SimpleCV import Camera, DrawingLayer
+from SimpleCV import Camera
 from numpy import fft
 import numpy
-from helpers import set_interval, sample_avg_green
+import helpers
+import matplotlib.pyplot as plt
+import time
 # RGB
 cam = Camera()
 SAMPLE_WIDTH = 100
@@ -15,42 +17,47 @@ sample_x0 = cam_center_point[0] - (SAMPLE_WIDTH / 2)
 sample_y0 =  cam_center_point[1] - (SAMPLE_HEIGHT / 2)
 green_values = []
 SAMPLE_RATE = 30 # same as the camera FPS
-
-# 0.75 - 2.5hz
-i = 0
-while i < 300:
+plt.ion() # enable interactive graphing
+plt.show()
+# # 0.75 - 2.5hz
+i = 0 
+while True:
     i += 1
+    if i % 10 ==0:
+        print "frame num %d" %i
     img = cam.getImage()
-    boxLayer = DrawingLayer((img.width, img.height))
-
-    boxLayer.centeredRectangle(cam_center_point, (SAMPLE_WIDTH, SAMPLE_HEIGHT))
     sampled_img = img[sample_x0:sample_x0 + SAMPLE_WIDTH, sample_y0: sample_y0 + SAMPLE_HEIGHT]
-    avg_green = sample_avg_green(sampled_img)
-
-    print "Green Average Val: %d" % avg_green
-
+    avg_green = helpers.sample_avg_green(sampled_img)
     green_values.append(avg_green)
 
-    img.addDrawingLayer(boxLayer)
-    img.applyLayers()
-    img.show()
+    # print "Green Average Val: %d" % avg_green
 
-del cam
-fft_val = fft.rfft(green_values)
-print fft_val
-stuff= map(lambda n: abs(n.real), fft_val)
-del stuff[0]    
-print stuff
+    helpers.draw_box_outline_and_show(img)
 
-# freq = fft.fftfreq(fft_val.size -1 , d=1./SAMPLE_RATE)
-# print freq
-# # save the output to the file
-# f = open('fft.txt', 'w')
-# f.write(str(stuff))
-# f.close
+    # This logic is wrong for stuff :/
+    if len(green_values) > 105: # 3.5 seconds of frames
+        if len(green_values) % 90 == 0: #every 3 seconds, process the most recent 3 seconds for frames
+            del green_values[:15] #delete the first half second of frames
 
-import matplotlib.pyplot as plt
-plt.plot(stuff)
-plt.show()
+            fft_val = fft.rfft(green_values)
+            # print fft_val
+            stuff= map(lambda n: abs(n.real), fft_val)
+            # print stuff
+            plt.clf()
+            potential_range = stuff[helpers.LHR_INDEX:helpers.UHR_INDEX]
+
+            #Say the highest value is the first number, since we sliced, we want to add the LHR index offset to the value (7)
+            # 0 + 7 = 7
+            # 7 * .1(NYQUST_FREQ / SAMPLE_SIZE) = .7
+            # .7 * 60(seconds in a minute) = 40 BPM
+            max_amp_index = max(zip(potential_range, range(len(potential_range))))
+            print max_amp_index
+            index = max_amp_index[1]  + helpers.LHR_INDEX 
+            
+            print "EST HB: %d" % (index * helpers.HZ_MAPPER * 60)
+            # print stuff[helpers.LHR_INDEX:helpers.UHR_INDEX]
+            plt.plot(stuff[helpers.LHR_INDEX:helpers.UHR_INDEX])
+            plt.draw()
+
 
 
--- helpers.py
+++ helpers.py
@@ -1,4 +1,47 @@
 import threading
+import SimpleCV
+
+
+SAMPLE_RATE = 30
+NYQUIST_FREQ = SAMPLE_RATE / 2 #is this actually the right name for a var
+
+# We're just going to make the assumption that folks are alive
+LOWER_HEART_RATE_BPM = 40        
+UPPER_HEART_RATE_BPM = 180
+
+LHR_HZ = LOWER_HEART_RATE_BPM / 60.0
+UHR_HZ = UPPER_HEART_RATE_BPM / 60.0
+
+# HZ_MAPPER = NYQUIST_FREQ / sample_size 
+HZ_MAPPER = 0.1
+# Map hz to bucket index
+LHR_INDEX = int(round(LHR_HZ * 10)) #.7hz
+UHR_INDEX = int(round(UHR_HZ * 10)) # 3 hz
+
+SAMPLE_WIDTH=100
+SAMPLE_HEIGHT=100
+
+PIXEL_COUNT = SAMPLE_WIDTH * SAMPLE_HEIGHT
+
+
+def sample_avg_green(img):
+    matrix = img.getNumpy()
+    total = 0
+    for row in range(0, 100):
+        for col in matrix[row]:
+            total += col[1] # Get the green val
+
+    return total / PIXEL_COUNT
+
+def draw_box_outline_and_show(img):
+    boxLayer = SimpleCV.DrawingLayer((img.width, img.height))
+    boxLayer.centeredRectangle((img.width/2, img.height / 2), (SAMPLE_WIDTH, SAMPLE_HEIGHT))
+    img.addDrawingLayer(boxLayer)
+    img.applyLayers()
+    img.show()
+
+
+
 
 # maybe replace this with greenlets?
 # http://greenlet.readthedocs.org/en/latest/
@@ -9,14 +52,3 @@ def set_interval(f, sec):
     t = threading.Timer(sec, func_wrapper)
     t.start() #this will get called once
     return t
-
-
-def sample_avg_green(img):
-    PIXEL_COUNT = 100 * 100
-    matrix = img.getNumpy()
-    total = 0
-    for row in range(0, 100):
-        for col in matrix[row]:
-            total += col[1] # Get the green val
-
-    return total / PIXEL_COUNT
